4e3a6cf8d7982d638a201be1c6a33f7397716de0
Kasy00 <kamilsyktus3@gmail.com>
2024-03-12 20:34:57 +0100

    Created homepage in own JS module

---
+++ src/content.js
@@ -0,0 +1,23 @@
+export function createHomepage(){
+    const content = document.getElementById('content');
+
+    const headline = document.createElement('h1');
+    headline.textContent = 'Welcome to our';
+
+    const nameHeadline = document.createElement('span');
+    nameHeadline.textContent = 'Trattoria';
+    nameHeadline.classList.add('name-highlight');
+
+    headline.appendChild(nameHeadline);
+    content.appendChild(headline);
+
+    const description = document.createElement('p');
+    description.textContent = 'Premium and authentic Italian cuisine';
+
+    content.appendChild(description);
+
+    const bookTableBtn = document.createElement('button');
+    bookTableBtn.textContent = 'Book table';
+
+    content.appendChild(bookTableBtn);
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './style.css';
+import { createHomepage } from './content';
 import Icon from './hero-bg.jpg';
+
+document.addEventListener('DOMContentLoaded', function() {
+    createHomepage();
+});
